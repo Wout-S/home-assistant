deef3f0caae98fbf55b7dc24140618fbe56b5dd5
Wout-S <35503601+Wout-S@users.noreply.github.com>
2018-12-22 17:08:52 +0100

    Discover as light to enable valve control

--- homeassistant/components/zwave/discovery_schemas.py
+++ homeassistant/components/zwave/discovery_schemas.py
@@ -84,6 +84,11 @@ DISCOVERY_SCHEMAS = [
                     ],
                     const.DISC_OPTIONAL: True,
                 },
+                'valve_state': {
+                    const.DISC_COMMAND_CLASS: [
+                        const.COMMAND_CLASS_SWITCH_MULTILEVEL],
+                    const.DISC_OPTIONAL: True,
+                },
                 "zxt_120_swing_mode": {
                     const.DISC_COMMAND_CLASS: [const.COMMAND_CLASS_CONFIGURATION],
                     const.DISC_INDEX: [33],
@@ -193,10 +198,12 @@ DISCOVERY_SCHEMAS = [
         const.DISC_COMPONENT: "light",
         const.DISC_GENERIC_DEVICE_CLASS: [
             const.GENERIC_TYPE_SWITCH_MULTILEVEL,
+            const.GENERIC_TYPE_THERMOSTAT,
             const.GENERIC_TYPE_SWITCH_REMOTE,
         ],
         const.DISC_SPECIFIC_DEVICE_CLASS: [
             const.SPECIFIC_TYPE_POWER_SWITCH_MULTILEVEL,
+            const.SPECIFIC_TYPE_THERMOSTAT_GENERAL_V2,
             const.SPECIFIC_TYPE_SCENE_SWITCH_MULTILEVEL,
             const.SPECIFIC_TYPE_NOT_USED,
         ],
